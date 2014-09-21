21601fc14f6e2760603b1f8040305938437091bc
Byron Henze <byronh@gmail.com>
2014-09-21 01:22:58 -0700

    Test C++11

--- engine/src/lib.cpp
+++ engine/src/lib.cpp
@@ -1,5 +1,11 @@
 #include <SFML/Graphics.hpp>
 #include <lib.h>
+#include <iostream>
+
+
+struct Foo {
+    int val = 123;
+};
 
 
 int helloFunc() {
@@ -8,14 +14,30 @@ int helloFunc() {
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
+    Foo f;
+    std::cout << f.val << std::endl;
+
+    std::vector<int> vec;
+    vec.push_back(3);
+    vec.push_back(2);
+    vec.push_back(1);
+
+    for (auto it = vec.begin(); it != vec.end(); ++it) {
+        std::cout << *it << std::endl;
+    }
+
     // run the program as long as the window is open
     while (window.isOpen()) {
         // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
         while (window.pollEvent(event)) {
             // "close requested" event: we close the window
-            if (event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed) {
                 window.close();
+            }
+            else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
+                window.close();
+            }
         }
 
         window.clear();
