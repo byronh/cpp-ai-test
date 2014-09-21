5df2f8823a15c0f4d4ee5c384b710a6fe5968296
Byron Henze <byronh@gmail.com>
2014-09-21 00:59:05 -0700

    SFML Hello World

--- engine/src/lib.cpp
+++ engine/src/lib.cpp
@@ -1,6 +1,27 @@
-#include "../include/lib.h"
+#include <SFML/Graphics.hpp>
+#include <lib.h>
 
 
 int helloFunc() {
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    // run the program as long as the window is open
+    while (window.isOpen()) {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
     return 42;
 }
